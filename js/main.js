0ff58a331888704ae797d36283cdbb2f3f34e3d7
damiao <damiaotenorio@gmail.com>
2020-01-11 13:04:43 -0300

    adiocionada pagina no e redirecionamento adicionado na principal

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
 function redirecionar(){
     window.location.href = "site/index.html";
 }
+function redirect(){
+    window.location.href = "no/no.html";
+}
---
+++ no/js/no.js
@@ -0,0 +1,3 @@
+function redirecionar(){
+    window.location.href = "https://damiao-santos.github.io/ray/site/index.html";
+}
